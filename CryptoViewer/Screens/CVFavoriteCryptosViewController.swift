d9ad62776b890551392842ddc712cf588401ba02
Andrii_Mac <andrystarik@gmail.com>
2024-08-27 21:27:35 +0300

    small design for tab bar

---
+++ CryptoViewer/Screens/CVFavoriteCryptosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CVFavoriteCryptosViewController.swift
+//  CryptoViewer
+//
+//  Created by Andrii Staryk on 27.08.2024.
+//
+
+import UIKit
+
+class CVFavoriteCryptosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CryptoViewer/Screens/CVTrendingCryptosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CVTrendingCryptosViewController.swift
+//  CryptoViewer
+//
+//  Created by Andrii Staryk on 27.08.2024.
+//
+
+import UIKit
+
+class CVTrendingCryptosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
